8ff84d4d301d297d6ea9dc27512eba0e506f3226
Ahmad Jawabra <ahmadjawabra95@gmail.com>
2021-09-01 19:45:50 +0300

    build the custom icon component

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,43 @@
 
   <h1>display icon demo</h1>
 
-  <fa icon="coffee" />
-  <fa :icon="['fas', 'coffee']" />
-  <fa icon="user" />
-  <fa icon="dove" />
+  <CustomIcon
+      iconName="search"
+      iconSize="4x"
+  />
+  <CustomIcon
+      iconName="edit"
+      iconSize="4x"
+  />
+  <CustomIcon
+      iconName="lightbulb"
+      iconSize="4x"
+  />
+  <CustomIcon
+      iconName="file-alt"
+      iconSize="4x"
+  />
+  <CustomIcon
+      iconName="tags"
+      iconSize="4x"
+  />
 
-  <fa :icon="['fab','youtube']" />
+  <CustomIcon
+      iconName="users"
+      iconSize="4x"
+  />
 
   <h1>display icon demo</h1>
 </template>
 
 <script>
 import TheHome from "./pages/TheHome";
+import CustomIcon from "./components/shared/CustomIcon"
 export default {
   name: 'App',
   components: {
-    TheHome
+    TheHome,
+    CustomIcon
   }
 }
 </script>
---
+++ src/components/shared/CustomIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <span class="icon-container">
+    <fa :icon="iconName" :size="iconSize" />
+  </span>
+</template>
+
+<script>
+export default {
+  name: "CustomIcon",
+  props: ['iconName', 'iconSize']
+}
+</script>
+
+<style scoped>
+
+</style>
