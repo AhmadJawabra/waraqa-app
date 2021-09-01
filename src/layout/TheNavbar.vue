12605e0d0c1d6ad428c612503dc451b435448855
Ahmad Jawabra <ahmadjawabra95@gmail.com>
2021-09-01 16:13:36 +0300

    build the button component.

--- src/layout/TheNavbar.vue
+++ src/layout/TheNavbar.vue
@@ -1,10 +1,15 @@
 <template>
+  <button class="btn btn--primary">تسجيل</button>
+  <button class="btn btn--outline">دخول</button>
 
+  <button class="btn btn--primary btn--block">تسجيل</button>
+
+  <button class="btn btn--transparent">دخول</button>
 </template>
 
 <script>
 export default {
-  name: "TheNavbar"
+  name: "TheNavbar",
 }
 </script>
 
