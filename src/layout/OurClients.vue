7d3abbccb7f129e8bb0fd06398b9bde3deee950c
Ahmad Jawabra <ahmadjawabra95@gmail.com>
2021-09-02 06:11:18 +0300

    update our clients section & remove redundant code.

--- src/layout/OurClients.vue
+++ src/layout/OurClients.vue
@@ -4,33 +4,37 @@
       <h2 class="mb-2 text-heading">عملاؤنا</h2>
     </header>
     <div class="grid grid--1x4">
-      <div class="client-wrapper">
-        <img src="@/assets/images/advisor.svg" alt="advisor logo">
-      </div><div class="client-wrapper">
-        <img src="@/assets/images/sotor.svg" alt="sotor logo">
-      </div><div class="client-wrapper">
-        <img src="@/assets/images/mawdoo3.svg" alt="mawdoo3 logo">
-      </div><div class="client-wrapper">
-        <img src="@/assets/images/life.svg" alt="life logo">
-      </div><div class="client-wrapper">
-        <img src="@/assets/images/life-red.svg" alt="life logo">
-      </div><div class="client-wrapper">
-        <img src="@/assets/images/supermama.svg" alt="supermama logo">
-      </div><div class="client-wrapper">
-        <img src="@/assets/images/health.svg" alt="health logo">
-      </div><div class="client-wrapper">
-        <img src="@/assets/images/advisor.svg" alt="advisor logo">
+      <div v-for="img in clientsLogo" class="client-wrapper">
+        <img :src="img.src" :alt="img.alt">
       </div>
     </div>
   </section>
 </template>
 
 <script>
+
+import advisor from '@/assets/images/advisor.svg';
+import sotor from "@/assets/images/sotor.svg";
+import mawdoo3 from "@/assets/images/mawdoo3.svg"
+import life from "@/assets/images/life.svg";
+import lifeRed from "@/assets/images/life-red.svg";
+import supermama from "@/assets/images/supermama.svg";
+import health from "@/assets/images/health.svg";
+
 export default {
   name: "OurClients",
   data() {
     return {
-
+      clientsLogo: [
+          { src: advisor, alt: 'advisor logo' },
+          { src: sotor, alt: 'sotor logo' },
+          { src: mawdoo3, alt: 'mawdoo3 logo' },
+          { src: life, alt: 'life logo' },
+          { src: lifeRed, alt: 'life logo' },
+          { src: supermama, alt: 'supermama logo' },
+          { src: health, alt: 'health logo' },
+          { src: advisor, alt: 'advisor logo' },
+      ]
     }
   }
 }
